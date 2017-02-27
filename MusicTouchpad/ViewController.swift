5796b8a048239580efffcb2b4bb7d8d3f1d30857
Ethan Jud <ethanj@Ethans-MacBook-Pro.local>
2017-02-27 15:06:39 +0700

    Initial Commit

---
+++ MusicTouchpad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicTouchpad
+//
+//  Created by Ethan Jud on 2/27/17.
+//  Copyright © 2017 Ethan Jud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
